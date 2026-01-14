c2f8d855ae4aaae34aa5528f9be7c8005929e6ed
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-01-14 11:46:08 +0300

    hallf

--- client/src/components/layout/PublicLayout.jsx
+++ client/src/components/layout/PublicLayout.jsx
@@ -1,12 +1,24 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import { Outlet, Link } from "react-router-dom";
 
-export default function PublicLayout({ children }) {
+export default function PublicLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1">{children}</main>
-      <Footer />
+    <div className="min-h-screen flex flex-col">
+      <nav className="bg-indigo-600 text-white p-4 flex justify-between">
+        <h1 className="font-bold text-xl">Event360</h1>
+        <div className="space-x-4">
+          <Link to="/" className="hover:underline">Home</Link>
+          <Link to="/login" className="hover:underline">Login</Link>
+          <Link to="/signup" className="hover:underline">Sign Up</Link>
+        </div>
+      </nav>
+
+      <main className="flex-1 p-6">
+        <Outlet />
+      </main>
+
+      <footer className="bg-gray-100 text-center p-4">
+        © 2026 Event360
+      </footer>
     </div>
   );
 }
